6b8eea151fac28b80bc77a563b13ad1a5723b34b
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2013-05-19 23:31:14 +0200

    Actualización de prueba

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -33,4 +33,28 @@ char MainWindow::calcularLetra(QString d){
    ui->letra->setText(QString("TRWAGMYFPDXBNJZSQVHLCKE"[d.toInt()%23]));
 }
 
-char MainWindow::calcularDC (QString d) {}
+char MainWindow::calcularDC (QString d) {
+    /*
+     // Todo correcto, calculamos el dígito de control
+    var numeroCuenta = f.entidad.value + "" + f.oficina.value + "--" + f.cuenta.value;
+
+    valores = new Array(1, 2, 4, 8, 5, 10, 9, 7, 3, 6);
+    var controlCS = 0;
+    var controlCC = 0;
+    for (i=0; i<=7; i++)
+    {controlCS += parseInt(numeroCuenta.charAt(i)) * valores[i+2];
+    }
+    controlCS = 11 - (controlCS % 11);
+    if (controlCS == 11) controlCS = 0;
+    else if (controlCS == 10) controlCS = 1;
+
+    for (i=10; i<=19; i++)
+    controlCC += parseInt(numeroCuenta.charAt(i)) * valores[i-10];
+    controlCC = 11 - (controlCC % 11);
+    if (controlCC == 11) controlCC = 0;
+    else if (controlCC == 10) controlCC = 1;
+
+    f.dc.value = controlCS + "" + controlCC;
+    */
+
+}
